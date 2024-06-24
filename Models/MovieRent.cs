df93c233dd7fcce91a81adcad122990484485e29
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-06-24 15:04:08 +0700

    feat: add movie-rent model

---
+++ Models/MovieRent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVCApplication.Models
+{
+    public class MovieRent
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+        public int CustomerId { get; set; }
+        public IdentityUser Customer { get; set; } = null!;
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; } 
+    }
+}
